6ef784e00540af975cb89b2e1e650da468427f02
NiazSagor <niazsagor@gmail.com>
2025-02-22 12:47:40 +0600

    show the daily leetcode problem in widget

--- app/src/main/kotlin/com/byteutility/dev/leetcode/plus/glance/ui/LeetcodePlusGlanceAppWidget.kt
+++ app/src/main/kotlin/com/byteutility/dev/leetcode/plus/glance/ui/LeetcodePlusGlanceAppWidget.kt
@@ -2,10 +2,14 @@ package com.byteutility.dev.leetcode.plus.glance.ui
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalContext
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
@@ -13,12 +17,17 @@ import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
 import androidx.glance.layout.Column
-import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import com.byteutility.dev.leetcode.plus.data.datastore.UserDatastore
+import com.byteutility.dev.leetcode.plus.data.model.LeetCodeProblem
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.components.SingletonComponent
 
 class LeetcodePlusGlanceAppWidget : GlanceAppWidget() {
 
@@ -27,36 +36,63 @@ class LeetcodePlusGlanceAppWidget : GlanceAppWidget() {
         id: GlanceId
     ) {
         provideContent {
-            Widget()
+            val context = LocalContext.current
+            val datastore = EntryPointAccessors.fromApplication(
+                context.applicationContext,
+                WidgetEntryPoint::class.java
+            ).userDatastore()
+            val dailyProblem = remember { mutableStateOf<LeetCodeProblem?>(null) }
+            var isLoading = remember { mutableStateOf(true) }
+            LaunchedEffect(Unit) {
+                datastore.getDailyProblem().collect {
+                    dailyProblem.value = it
+                    isLoading.value = false
+                }
+            }
+            if (isLoading.value) {
+
+            } else {
+                dailyProblem.value?.let {
+                    Widget(it)
+                }
+            }
         }
     }
 
     @Composable
-    private fun Widget() {
+    private fun Widget(
+        problem: LeetCodeProblem
+    ) {
         Box(
             modifier = GlanceModifier
-                .padding(horizontal = 8.dp, vertical = 8.dp)
-                .fillMaxWidth()
-                .cornerRadius(12.dp)
-                .background(Color(0xFF4CAF50))
+                .background(Color.White)
         ) {
-            Row(
-                modifier = GlanceModifier.padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically,
+            Box(
+                modifier = GlanceModifier
+                    .padding(8.dp)
+                    .background(Color(0xFF4CAF50)) // Green color
+                    .fillMaxWidth()
+                    .cornerRadius(12.dp)
             ) {
                 Column(
-                    modifier = GlanceModifier.defaultWeight(),
-                    horizontalAlignment = Alignment.Start
+                    horizontalAlignment = Alignment.Start,
                 ) {
                     Text(
                         text = "LeetCode Daily Problem",
                     )
                     Spacer(modifier = GlanceModifier.height(4.dp))
                     Text(
-                        text = "Solve today for a streak!",
+                        text = problem.title,
                     )
                 }
             }
         }
     }
+}
+
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface WidgetEntryPoint {
+    fun userDatastore(): UserDatastore
 }
