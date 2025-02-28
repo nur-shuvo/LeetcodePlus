d192c1faf1b542eb51a2e5ccb8b5ebd6c1aea1df
Shuvo <nurshuvo51@gmail.com>
2025-02-28 22:12:59 +0600

    Customize layouts

--- app/src/main/kotlin/com/byteutility/dev/leetcode/plus/glance/ui/LeetcodePlusGlanceAppWidget.kt
+++ app/src/main/kotlin/com/byteutility/dev/leetcode/plus/glance/ui/LeetcodePlusGlanceAppWidget.kt
@@ -1,16 +1,23 @@
 package com.byteutility.dev.leetcode.plus.glance.ui
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
@@ -21,7 +28,10 @@ import androidx.glance.layout.Spacer
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.height
 import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
 import com.byteutility.dev.leetcode.plus.data.datastore.UserDatastore
 import com.byteutility.dev.leetcode.plus.data.model.LeetCodeProblem
 import dagger.hilt.EntryPoint
@@ -39,7 +49,7 @@ class LeetcodePlusGlanceAppWidget : GlanceAppWidget() {
             val context = LocalContext.current
             val datastore = EntryPointAccessors.fromApplication(
                 context.applicationContext,
-                WidgetEntryPoint::class.java
+                UserDataStoreProviderEntryPoint::class.java
             ).userDatastore()
             val dailyProblem = remember { mutableStateOf<LeetCodeProblem?>(null) }
             var isLoading = remember { mutableStateOf(true) }
@@ -53,36 +63,52 @@ class LeetcodePlusGlanceAppWidget : GlanceAppWidget() {
 
             } else {
                 dailyProblem.value?.let {
-                    Widget(it)
+                    DailyProblemWidget(it)
                 }
             }
         }
     }
 
     @Composable
-    private fun Widget(
+    private fun DailyProblemWidget(
         problem: LeetCodeProblem
     ) {
+        val intent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("https://leetcode.com/problems/${problem.titleSlug}/description")
+        )
+
         Box(
             modifier = GlanceModifier
-                .background(Color.White)
+                .padding(8.dp)
+                .background(ColorProvider(Color.White))
+                .fillMaxWidth()
+                .cornerRadius(16.dp)
         ) {
             Box(
                 modifier = GlanceModifier
-                    .padding(8.dp)
-                    .background(Color(0xFF4CAF50)) // Green color
+                    .padding(12.dp)
+                    .background(MaterialTheme.colorScheme.errorContainer)
                     .fillMaxWidth()
                     .cornerRadius(12.dp)
+                    .clickable(actionStartActivity(intent))
             ) {
                 Column(
                     horizontalAlignment = Alignment.Start,
                 ) {
                     Text(
-                        text = "LeetCode Daily Problem",
+                        text = "Problem of the day",
+                        style = TextStyle(
+                            fontSize = TextUnit(16.0F, TextUnitType.Sp)
+                        )
                     )
                     Spacer(modifier = GlanceModifier.height(4.dp))
                     Text(
                         text = problem.title,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = TextUnit(20.0F, TextUnitType.Sp)
+                        )
                     )
                 }
             }
@@ -93,6 +119,6 @@ class LeetcodePlusGlanceAppWidget : GlanceAppWidget() {
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
-interface WidgetEntryPoint {
+interface UserDataStoreProviderEntryPoint {
     fun userDatastore(): UserDatastore
 }
