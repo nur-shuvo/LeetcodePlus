4cadb0c1c19d5cd2896025750a0bd07673728be0
Nur Shuvo <nurshuvo51@gmail.com>
2025-10-10 19:36:54 +0600

    update app version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.byteutility.dev.leetcode.plus"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
