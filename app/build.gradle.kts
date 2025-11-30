29a731d863aff8d7f74fd805020c40e899a0ec43
Nur Shuvo <nurshuvo51@gmail.com>
2025-11-30 11:37:09 +0600

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.byteutility.dev.leetcode.plus"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
