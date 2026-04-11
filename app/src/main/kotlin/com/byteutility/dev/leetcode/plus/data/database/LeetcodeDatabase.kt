0eb4d855a1cd617902ed629c54e7ddff8c2a5ef0
Johny <rashid.mamunur@pathao.com>
2026-04-11 22:02:40 +0600

    change database version and migration policy 2_3

--- app/src/main/kotlin/com/byteutility/dev/leetcode/plus/data/database/LeetcodeDatabase.kt
+++ app/src/main/kotlin/com/byteutility/dev/leetcode/plus/data/database/LeetcodeDatabase.kt
@@ -43,7 +43,19 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
 val MIGRATION_2_3 = object : Migration(2, 3) {
     override fun migrate(db: SupportSQLiteDatabase) {
         db.execSQL("DROP TABLE IF EXISTS `solved_problem`")
-        db.execSQL("CREATE TABLE IF NOT EXISTS `all_problems_new` (`problem_id` INTEGER NOT NULL PRIMARY KEY, `title` TEXT NOT NULL, `title_slug` TEXT, `difficulty` TEXT NOT NULL, `acceptance` TEXT NOT NULL DEFAULT '', `isPaidOnly` INTEGER NOT NULL, `has_solution` INTEGER NOT NULL, `has_video_solution` INTEGER NOT NULL, `topic_tags` TEXT)")
+        db.execSQL("""
+            CREATE TABLE IF NOT EXISTS `all_problems_new` (
+                `problem_id` INTEGER NOT NULL PRIMARY KEY,
+                `title` TEXT NOT NULL,
+                `title_slug` TEXT,
+                `difficulty` TEXT NOT NULL,
+                `acceptance` TEXT NOT NULL,
+                `isPaidOnly` INTEGER NOT NULL,
+                `has_solution` INTEGER NOT NULL,
+                `has_video_solution` INTEGER NOT NULL,
+                `topic_tags` TEXT
+            )
+        """)
         db.execSQL("INSERT INTO `all_problems_new` (`problem_id`, `title`, `title_slug`, `difficulty`, `acceptance`, `isPaidOnly`, `has_solution`, `has_video_solution`, `topic_tags`) SELECT `problem_id`, `title`, `title_slug`, `difficulty`, CAST(`acceptance` AS TEXT), `isPaidOnly`, `has_solution`, `has_video_solution`, `topic_tags` FROM `all_problems`")
         db.execSQL("DROP TABLE `all_problems`")
         db.execSQL("ALTER TABLE `all_problems_new` RENAME TO `all_problems`")
